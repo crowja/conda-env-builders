3b35e3e2b60055dcf2beccd351384c66d9f99c8f
jac <crowja@gmail.com>
2020-11-21 16:07:45 -0600

    Typo with -n.

--- conda-create-biopython-pandas-py3.bash
+++ conda-create-biopython-pandas-py3.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-conda create -b biopython-pandas-py3 -c conda-forge -c bioconda \
+conda create -n biopython-pandas-py3 -c conda-forge -c bioconda \
 biopython \
 black \
 bump2version \
